77e6ee56913ca3f534318e15b4bb885c6b950709
6ftffyq3 <49231284+6ftffyq3@users.noreply.github.com>
2025-07-23 09:15:19 +0000

    use local respository for android build

--- ports/ci/android/install_deps.sh
+++ ports/ci/android/install_deps.sh
@@ -92,6 +92,40 @@ chown -R aurbuild:aurbuild /tmp/aurbuild
 su - aurbuild -c "git clone https://aur.archlinux.org/yay.git /tmp/aurbuild/yay"
 su - aurbuild -c "cd /tmp/aurbuild/yay && makepkg -si --noconfirm"
 
+# Download local Android binary repository
+
+${DOWNLOAD_CMD} https://github.com/6ftffyq3/static/releases/download/webcamoid/arch-repo-local-packages_split.7z.001
+${DOWNLOAD_CMD} https://github.com/6ftffyq3/static/releases/download/webcamoid/arch-repo-local-packages_split.7z.002
+7z x arch-repo-local-packages_split.7z.001
+rm -f arch-repo-local-packages_split.7z.*
+7z x -oarch-repo/ arch-repo-local-packages.7z
+
+# Check the db file
+
+ls "${PWD}/arch-repo/local-packages/os/any/local-packages.db"
+
+# Asign the local repository to the alpm group
+
+chown :alpm -Rf "${PWD}/arch-repo"
+
+# Map the directory to a local server
+
+nohup python -m http.server --directory "${PWD}/arch-repo" &
+sleep 10s
+cat ~/nohup.out || true
+
+# Configure local Android binary repository
+
+cat << EOF >> /etc/pacman.conf
+
+[local-packages]
+SigLevel = Never
+Server = http:///localhost:8000/local-packages/os/any
+EOF
+sed -i 's/Required DatabaseOptional/Never/g' /etc/pacman.conf
+
+pacman -Syy
+
 # Configure Java
 
 archlinux-java status
